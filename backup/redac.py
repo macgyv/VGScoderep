a5cb7d13d182ba5e932a398a7453dd05d007d27b
Oliver <32289950+macgyv@users.noreply.github.com>
2022-03-26 06:18:59 +0800

    backup and yaml

---
+++ backup/redac.py
@@ -0,0 +1,5 @@
+import requests
+
+response = requests.post("https://tntsfeqzp4a.sandbox.verygoodproxy.com/post",
+                          json={'account_number': 'ACC00000000000000000'})
+print(str(response.text))
