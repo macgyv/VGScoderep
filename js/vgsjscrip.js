934d77be01eab7c8a32083ace7692b8cf953d10c
Oliver <32289950+macgyv@users.noreply.github.com>
2022-03-26 07:37:13 +0800

    css and js

---
+++ js/vgsjscrip.js
@@ -0,0 +1,57 @@
+const vgsForm = window.VGSCollect.create('tntsfeqzp4a', 'sandbox', () => {});
+
+const css = {
+  boxSizing: 'border-box',
+  fontFamily: '-apple-system, BlinkMacSystemFont, "Segoe UI"',
+};
+
+vgsForm.field('#cc-holder', {
+  type: 'text',
+  name: 'card_holder',
+  placeholder: 'Card holder',
+  validations: ['required'],
+  css: css,
+});
+
+vgsForm.field('#cc-number', {
+  type: 'card-number',
+  name: 'card_number',
+  successColor: '#4F8A10',
+  errorColor: '#D8000C',
+  placeholder: 'Card number',
+  showCardIcon: true,
+  validations: ['required', 'validCardNumber'],
+  css: css,
+});
+
+vgsForm.field('#cc-cvc', {
+  type: 'card-security-code',
+  name: 'card_cvc',
+  successColor: '#4F8A10',
+  errorColor: '#D8000C',
+  placeholder: 'CVC',
+  maxLength: 3,
+  validations: ['required', 'validCardSecurityCode'],
+  css: css,
+});
+
+vgsForm.field('#cc-expiration-date', {
+  type: 'card-expiration-date',
+  name: 'card_exp',
+  successColor: '#4F8A10',
+  errorColor: '#D8000C',
+  placeholder: 'MM / YY',
+  validations: ['required', 'validCardExpirationDate'],
+  css: css,
+});
+
+document.getElementById("collect-form").addEventListener('submit', (e) => {
+  e.preventDefault();
+  vgsForm.submit('/post', {}, (status, response) => {
+    if (status === 200) {
+      console.log("success");
+    }
+  }, (error) => {
+    console.log(error);
+  });
+});
