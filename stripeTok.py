ac750d53b0cd99b27ee337485edfa49c0a174406
Oliver <32289950+macgyv@users.noreply.github.com>
2022-03-26 12:20:48 +0800

    CreditCardSample update

--- stripeTok.py
+++ stripeTok.py
@@ -1,11 +1,2 @@
 import stripe
-stripe.api_key = "sk_test_51KhPTvBI1KCMZju3sTZv5Zd6RryOKnmug0gqFBSrlFDP2gC74sKaKHN3Ay0eooD4wJusG9KuchKC7FJzhd360pBB00NdT5zgAD"
-
-stripe.Token.create(
-  card={
-    'card_holder': 'Oliver Pajarillo',
-           'card_number': '4242424242424242',
-           'card_cvc': '123',
-           'card_exp': '03/28',
-  },
-)
+stripe.api_key = "sk_test_4eC39HqLyjWDarjtT1zdp7dc"
