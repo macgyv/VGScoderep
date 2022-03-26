90a0ea4b9d4d1012b62c644e993dc9a6c679aef1
Oliver <32289950+macgyv@users.noreply.github.com>
2022-03-26 10:12:07 +0800

    update

--- redac.py
+++ redac.py
@@ -1,7 +1,8 @@
 import requests
 
 response = requests.post("https://tnte6doqz8w.sandbox.verygoodproxy.com/post",
-                          json={ 'card_holder': '4242424242424242',
+                          json={ 'card_holder': 'Oliver Pajarillo',
+                                 'card_number': '4242424242424242',
                                  'card_cvc': '123',
                                  'card_exp': '03/28'})
 print(str(response.text))
--- reveal.py
+++ reveal.py
@@ -3,9 +3,10 @@ import requests
 
 os.environ['HTTPS_PROXY'] = 'http://USiyQvWcT7wcpy8gvFb1GVmz:2b48a642-615a-4b3c-8db5-e02a88147174@tntsfeqzp4a.sandbox.verygoodproxy.com:8080'
 response = requests.post('https://echo.apps.verygood.systems/post',
-                         json={'card_holder': 'tok_sandbox_w8CBfH8vyYL2xWSmMWe3Ds',
-                               "card_cvc": "tok_sandbox_6ZRZo57qL2tYDHBhADuDo9",
-                               "card_exp": "tok_sandbox_rQkM2oVJDwzoQDonxw2kM8", 
-                               "card_holder": "tok_sandbox_hdKRcUn93ykSPqvJEC1AZR"},
+                         json={"card_holder": "tok_sandbox_nGhsG7Es6QD3GgySC76t1u",
+                               "card_number": "tok_sandbox_cfFUXBMRPRtWHa2pyy5WVu",
+                                "card_cvc": "tok_sandbox_6ZRZo57qL2tYDHBhADuDo9",
+                                "card_exp": "tok_sandbox_7EPXLtdQJKcuabrspYNQyb"
+                                },
                          verify='certs/sandbox.pem')
 print(str(response.text))
