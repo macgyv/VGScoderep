eae45c66ba4c3b055639c7656cb324b073836849
Oliver <32289950+macgyv@users.noreply.github.com>
2022-03-26 17:10:31 +0800

    Update reveal.py

--- reveal.py
+++ reveal.py
@@ -1,12 +1,13 @@
 import os
 import requests
 
+
 os.environ['HTTPS_PROXY'] = 'http://USiyQvWcT7wcpy8gvFb1GVmz:2b48a642-615a-4b3c-8db5-e02a88147174@tntsfeqzp4a.sandbox.verygoodproxy.com:8080'
 response = requests.post('https://echo.apps.verygood.systems/post',
-                         json={"card_holder": "tok_sandbox_nGhsG7Es6QD3GgySC76t1u",
-                               "card_number": "tok_sandbox_cfFUXBMRPRtWHa2pyy5WVu",
-                                "card_cvc": "tok_sandbox_6ZRZo57qL2tYDHBhADuDo9",
-                                "card_exp": "tok_sandbox_7EPXLtdQJKcuabrspYNQyb"
+                         json={"card_holder": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
+                               "card_number": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
+                                "card_cvc": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
+                                "card_exp": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM"
                                 },
                          verify='certs/sandbox.pem')
 print(str(response.text))
