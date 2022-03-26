b6a61ed1048005bdf3f8ce52268891f8a7350e0c
Oliver <32289950+macgyv@users.noreply.github.com>
2022-03-26 19:06:56 +0800

    update ccsample and reveal

--- reveal.py
+++ reveal.py
@@ -4,7 +4,7 @@ import requests
 
 os.environ['HTTPS_PROXY'] = 'http://USrD8iSwgj98rX49amoGYiqk:500edfdf-0831-4105-a512-a7d3fd1e9c9d@tnte6doqz8w.sandbox.verygoodproxy.com:8080'
 response = requests.post('https://tnte6doqz8w.sandbox.verygoodproxy.com/post',
-                         json={"card_holder": "tok_sandbox_5sCQ2zmHN4LRSwxk95tR2s",
+                         json={"card_holder": "tok_sandbox_xAxE4mH7NM3pdQP5YiqzWD",
                                "card_number": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
                                 "card_cvc": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
                                 "card_exp": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM"
