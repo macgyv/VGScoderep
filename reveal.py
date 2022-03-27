ede7e0d33bfd8cb1940a77acbfa22d8f716e9a27
Oliver <32289950+macgyv@users.noreply.github.com>
2022-03-27 16:32:56 +0800

    Creds and Reveal update

--- reveal.py
+++ reveal.py
@@ -3,11 +3,11 @@ import requests
 
 
 os.environ['HTTPS_PROXY'] = 'http://USrD8iSwgj98rX49amoGYiqk:500edfdf-0831-4105-a512-a7d3fd1e9c9d@tnte6doqz8w.sandbox.verygoodproxy.com:8080'
-response = requests.post('https://tnte6doqz8w.sandbox.verygoodproxy.com/post',
-                         json={"card_holder": "tok_sandbox_xAxE4mH7NM3pdQP5YiqzWD",
-                               "card_number": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
-                                "card_cvc": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
-                                "card_exp": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM"
+response = requests.post('https://echo.apps.verygood.systems/post',
+                         json={"card_cvc": "tok_sandbox_f83HNxhZBgrAywnNTFvwpw",
+    "card_exp": "tok_sandbox_7ipuvMaUtLVePy2Kw17vKY",
+    "card_holder": "tok_sandbox_a975PkDRGeRqMbMPheSfou",
+    "card_number": "tok_sandbox_svYnZ6VGheiSPBV6Rf5Ffd"
                                 },
                          verify='certs/sandbox.pem')
 print(str(response.text))
@@ -17,5 +17,10 @@ echo.apps.verygood.systems
 api.stripe.com
 
 https://tnte6doqz8w.sandbox.verygoodproxy.com/post
+
+"card_holder": "tok_sandbox_xAxE4mH7NM3pdQP5YiqzWD",
+      "card_number": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
+       "card_cvc": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM",
+       "card_exp": "tok_sandbox_tAk4ChGh7aGxgmmcKbxufM"
 
 """
